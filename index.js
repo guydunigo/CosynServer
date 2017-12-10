8f04f464391fe490d85321d5c59e0cc27847aed3
GONI Guillaume <gdn3@live.fr>
2017-12-10 15:15:15 +0100

    Added express module + base server + configs

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 4300;
+
+app.get('/', (request, response) => response.send('Hello dear students !'));
+
+// ----- Router -----
+const todoRouter = express.Router();
+todoRouter.route('/')
+    .get((request, response) => response.send('Get list todos'))
+    .post((request, response) => response.send('Add a todo'))
+    .put((request, response) => response.send('edit a todo'))
+    .delete((request, response) => response.send('edit a todo'));
+
+app.use('/todo', todoRouter);
+// ----------
+
+// ----- Display static folder as public -----
+app.use('/static',express.static('public'));
+// ----------
+app.get('/static', (req, res) => res.redirect('static/index.html'));
+
+app.listen(PORT, () => console.log('Web app started on port ' + PORT + '. '));
