9d97ca561fd7344310282c1f625013a90a9bf510
KlerviCS <klervi_bzh@live.fr>
2017-12-17 15:57:28 +0100

    add route : toggle, volume, play > get kb, get key

--- db-play-router.js
+++ db-play-router.js
@@ -28,15 +28,46 @@ dbPlayRouter.route('/:keyBoard_id')
         response.setHeader('Access-Control-Allow-Origin', '*');
         Database(db =>
             db.collection('playKeyBoards')
-                .find({id:request.params.keyBoard_id})
-                .then(boards => response.json(boards).send()) // fonctionne ??  > find not a function
+                .findOne({id: request.params.keyBoard_id})
+                .then(boards => response.json(boards).send())
         )
     })
     .delete((request, response)=>{
         response.setHeader('Access-Control-Allow-Origin', '*');
         Database(db =>
             db.collection('playKeyBoards')
-                .findOneAndDelete({id:request.params.keyBoard_id}))
+                .findOneAndDelete({id:request.params.keyBoard_id})
+                .then(kb => response.json(kb).send()));
     })
+
+dbPlayRouter.route('/:keyBoard_id/keys/:key_id')
+    .get((request, response) => {
+        Database(db =>
+            db.collection('playKeyBoards')
+                .findOne({id: request.params.keyBoard_id})
+                .then(kb => {
+                        const key = kb.keys.find(key => key.id === request.params.key_id)
+                        response.json(key).send();
+                })
+        );
+    });
+
+dbPlayRouter.route('/:keyBoard_id/keys/:key_id/toggle')
+    .put((request, response) => {
+        Database(db =>
+            db.collection('playKeyBoards')
+                .updateOne({id: request.params.keyBoard_id, "keys.id" : request.params.key_id}, {$set: {"keys.$.enabled": true}})
+        );
+    });
+
+dbPlayRouter.route('/:keyBoard_id/keys/:key_id/volume')
+    .put((request, response) => {
+        console.log(request.body.volume);
+        Database(db =>
+            db.collection('playKeyBoards')
+                .updateOne({id: request.params.keyBoard_id, "keys.id" : request.params.key_id}, {$set: {"keys.$.volume": Number.parseInt(request.body.volume)}})
+        );
+    });
+
 
 exports.dbPlayRouter = dbPlayRouter;
