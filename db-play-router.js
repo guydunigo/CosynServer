dca09484ffabee34281ca034f49ca6c98512f0a9
GONI Guillaume <gdn3@live.fr>
2017-12-18 21:27:18 +0100

    added long-polling for keys

--- db-play-router.js
+++ db-play-router.js
@@ -2,6 +2,23 @@ const express = require('express');
 const { Database, PLAY_COL } = require('./db');
 
 const dbPlayRouter = express.Router();
+const TIMEOUT = 10000;
+
+const getKeyboard = function (keyBoard_id) {
+    return Database(db =>
+        db.collection(PLAY_COL)
+            .findOne({ id: keyBoard_id })
+    );
+};
+
+let responsesTab = {};
+const respondToPolls = function (keyBoard_id) {
+    getKeyboard(keyBoard_id)
+        .then(kb => {
+            while (responsesTab[keyBoard_id].length > 0)
+                responsesTab[keyBoard_id].pop().json(kb.keys);
+        });
+};
 
 dbPlayRouter.route('/reset')
     .get((req, res) => {
@@ -28,11 +45,8 @@ dbPlayRouter.route('/')
 
 dbPlayRouter.route('/:keyBoard_id')
     .get((request, response) => {
-        Database(db =>
-            db.collection(PLAY_COL)
-                .findOne({ id: request.params.keyBoard_id })
-                .then(boards => response.json(boards))
-        );
+        getKeyboard(request.params.keyBoard_id)
+            .then(kb => response.json(kb));
     })
     .delete((request, response) => {
         Database(db =>
@@ -42,23 +56,23 @@ dbPlayRouter.route('/:keyBoard_id')
     });
 
 dbPlayRouter.route('/:keyBoard_id/keys/:key_id')
-    .get((request, response) => {
-        Database(db =>
-            db.collection(PLAY_COL)
-                .findOne({ id: request.params.keyBoard_id })
-                .then(kb => {
-                    const key = kb.keys.find(key => key.id === request.params.key_id);
-                    response.json(key);
-                })
-        );
-    });
+    .get((request, response) =>
+        getKeyboard(request.params.keyBoard_id)
+            .then(kb => {
+                const key = kb.keys.find(key => key.id === request.params.key_id);
+                response.json(key);
+            })
+    );
 
 dbPlayRouter.route('/:keyBoard_id/keys/:key_id/press')
     .put((request, response) => {
         Database(db =>
             db.collection(PLAY_COL)
                 .updateOne({ id: request.params.keyBoard_id, 'keys.id': request.params.key_id }, { $set: { 'keys.$.enabled': true } })
-                .then(() => response.json({ enabled: true }))
+                .then(() => {
+                    response.json({ enabled: true });
+                    respondToPolls(request.params.keyBoard_id);
+                })
         );
     });
 
@@ -67,7 +81,10 @@ dbPlayRouter.route('/:keyBoard_id/keys/:key_id/release')
         Database(db =>
             db.collection(PLAY_COL)
                 .updateOne({ id: request.params.keyBoard_id, 'keys.id': request.params.key_id }, { $set: { 'keys.$.enabled': false } })
-                .then(() => response.json({ enabled: false }))
+                .then(() => {
+                    response.json({ enabled: false });
+                    respondToPolls(request.params.keyBoard_id);
+                })
         );
     });
 
@@ -77,9 +94,26 @@ dbPlayRouter.route('/:keyBoard_id/keys/:key_id/volume')
         Database(db =>
             db.collection(PLAY_COL)
                 .updateOne({ id: request.params.keyBoard_id, 'keys.id': request.params.key_id }, { $set: { 'keys.$.volume': Number.parseInt(request.body.volume) } })
-                .then(() => response.json({ volume: request.body.volume }))
+                .then(() => {
+                    response.json({ volume: request.body.volume });
+                    respondToPolls(request.params.keyBoard_id);
+                })
         );
     });
 
+dbPlayRouter.route('/:keyBoard_id/keys_poll')
+    .get((request, response) => {
+        if (responsesTab[request.params.keyBoard_id])
+            responsesTab[request.params.keyBoard_id].push(response);
+        else
+            responsesTab[request.params.keyBoard_id] = [response];
+
+        response.setTimeout(TIMEOUT,
+            () => {
+                responsesTab[request.params.keyBoard_id] = responsesTab[request.params.keyBoard_id].filter(res => res !== response);
+                getKeyboard(request.params.keyBoard_id)
+                    .then(kb => response.json(kb.keys));
+            });
+    });
 
 exports.dbPlayRouter = dbPlayRouter;
